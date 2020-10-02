9d7f997a15df4295e7604ecd14162042b4fa695a
maulika27 <68596398+maulika27@users.noreply.github.com>
2020-10-02 15:53:59 +0530

    Update particle.js

--- particle.js
+++ particle.js
@@ -9,6 +9,8 @@ class Particle {
         this.body = Bodies.circle(x, y, this.r,options);       
 		this.color=color(random(0, 255), random(0, 255), random(0, 255));
 		this.body.velocityY = 5;
+	    
+    World.add(world, this.body);
       
 
     }
